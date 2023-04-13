dba9f35c61e0194fa9baa39f68cc4380e333e74f
Matheus <matheussantosmoura2000@gmail.com>
2023-04-13 16:20:35 -0300

    Expectativa de vida

--- vidabrasil/src/main/java/br/ufes/inf/jpalmeida/owid/App.java
+++ vidabrasil/src/main/java/br/ufes/inf/jpalmeida/owid/App.java
@@ -1,13 +1,31 @@
 package br.ufes.inf.jpalmeida.owid;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+        try {
+            Reader in = new FileReader("/home-temp/aluno/Documents/AulaMaven03/Vida-Brasil/vidabrasil/src/main/java/br/ufes/inf/jpalmeida/owid/female-and-male-life-expectancy-at-birth-in-years.csv");
+            Iterable<CSVRecord> records = CSVFormat.EXCEL.withDelimiter(',').parse(in);
+            for (CSVRecord record : records) {
+                String columnOne = record.get(0);
+                String columnTwo = record.get(1);
+                System.out.println(columnOne);
+                System.out.println(columnTwo);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Erro");
+        }
+
     }
 }
